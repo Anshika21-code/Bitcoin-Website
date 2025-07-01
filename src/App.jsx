e488bda295061db06934e5f4e88092fe54f53f93
Anshika21-code <143320200+Anshika21-code@users.noreply.github.com>
2025-07-02 02:11:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
 
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Coin from './pages/Coin/Coin'
+import Footer from './components/Navbar/Footer/Footer'
+import Features from './pages/Feature/Feature'
+import Pricing from './pages/Pricing/Pricing'
+import Contact from './pages/Contact/Contact'
+
+
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/coin/:coinId' element={<Coin />} />
+        <Route path='/features' element={<Features />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/contact' element={<Contact />} />
+        
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
